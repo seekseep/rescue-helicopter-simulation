f6cc361fb8edfb70a18ce5e09f24699a954ad45d
seekseep <seekseep@gmail.com>
2020-01-31 17:45:55 +0900

    主力ログの修正

--- src/ts/simulator/builders/texts.ts
+++ src/ts/simulator/builders/texts.ts
@@ -21,22 +21,42 @@ export const taskTypeToLabel = (taskType: TaskTyep): string => {
 
 export const result = (project: Project, environment: Environment): string => {
   const { shelterAgents, baseAgents, helicopterAgents } = environment
+
+  const { allRequestedInjuredsCount, allRescuedInjuredsCount } = shelterAgents.reduce(({ allRequestedInjuredsCount, allRescuedInjuredsCount },{ rescuedInjuredsCount, requestedInjuredsCount }) => {
+    return {
+      allRequestedInjuredsCount: allRequestedInjuredsCount + requestedInjuredsCount,
+      allRescuedInjuredsCount: allRescuedInjuredsCount + rescuedInjuredsCount,
+    }
+  }, {
+    allRequestedInjuredsCount: 0,
+    allRescuedInjuredsCount: 0
+  } )
+
   return utils.messagesToString([
-    'プロジェクト',
+    '概要',
     [
-      `開始日時: ${project.startedAt.toLocaleString()}`,
-      `終了日時: ${project.finishedAt.toLocaleString()}`
-    ],
-    '被災地概要',
-    shelterAgents.map(({ displayName, injuredsCount, rescuedInjuredsCount, requestedInjuredsCount, rescueRate }) => ([
-      displayName,
+      'プロジェクト',
       [
-        `要請数: ${requestedInjuredsCount}`,
-        `救助済み: ${rescuedInjuredsCount}`,
-        `未救助: ${injuredsCount}`,
-        `救助率: ${rescueRate * 100}%`
-      ]
-    ])),
+        `開始日時: ${project.startedAt.toLocaleString()}`,
+        `終了日時: ${project.finishedAt.toLocaleString()}`,
+        `要請救助者数: ${allRequestedInjuredsCount}`,
+        `救助済み: ${allRescuedInjuredsCount}`,
+      ],
+      '被災地',
+      shelterAgents.map(({ displayName, injuredsCount, rescuedInjuredsCount, requestedInjuredsCount, rescueRate }) => ([
+        displayName,
+        [
+          `救助済み: ${rescuedInjuredsCount}`,
+        ]
+      ])),
+      'ヘリコプター',
+      helicopterAgents.map(({ displayName, rescuedInjuredsCount }) => ([
+        displayName,
+        [
+          `救助済み: ${rescuedInjuredsCount}`,
+        ]
+      ]),
+    ],
     'ヘリコプター',
     helicopterAgents.map(({ displayName, missions, rescuedInjuredsCount }) => (
       [
@@ -49,17 +69,9 @@ export const result = (project: Project, environment: Environment): string => {
             `${startedAt.toLocaleString()}@${startedIn.displayName} → ${finishedAt.toLocaleString()}@${finishedIn.displayName}`,
             mission.tasks.map(({ type, startedIn, startedAt, finishedIn, finishedAt, injuredsCount }) => {
               const messages = []
-
-              if (injuredsCount) {
-                messages.push(`負傷者数:${injuredsCount}`)
-              }
-
-              if (startedIn.id !== finishedIn.id) {
-                messages.push(`距離: ${utils.distance(startedIn.position, finishedIn.position)}m`)
-              }
-
+              if (injuredsCount) messages.push(`負傷者数:${injuredsCount}`)
+              if (startedIn.id !== finishedIn.id) messages.push(`距離: ${utils.distance(startedIn.position, finishedIn.position)}m`)
               messages.push(`${startedAt.toLocaleString()}@${startedIn.displayName} → ${finishedAt.toLocaleString()}@${finishedIn.displayName}`)
-
               return [
                 `${taskTypeToLabel(type)} (${utils.ceilTime(utils.diffDates(startedAt, finishedAt)) / MINUTE}分)`,
                 messages
@@ -79,21 +91,22 @@ export const result = (project: Project, environment: Environment): string => {
             `${startedAt.toLocaleString()} → ${finishedAt.toLocaleString()}`,
             mission.tasks.map(({ type, startedAt, finishedAt, transport, injuredsCount }) => {
               const messages = [taskTypeToLabel(type)]
-
-              if (injuredsCount) {
-                messages.push(`負傷者数: ${injuredsCount}`)
-              }
-
+              if (injuredsCount) messages.push(`負傷者数: ${injuredsCount}`)
               messages.push(`${startedAt.toLocaleString()} → ${finishedAt.toLocaleString()} (${transport.displayName})`)
-
               return messages
             })
         ]
       })
     ])),
     '被災地',
-    shelterAgents.map(({ displayName, missions }) => ([
+    shelterAgents.map(({ displayName, missions, requestedInjuredsCount, rescuedInjuredsCount, injuredsCount, rescueRate }) => ([
       displayName,
+      [
+        `要請数: ${requestedInjuredsCount}`,
+        `救助済み: ${rescuedInjuredsCount}`,
+        `未救助: ${injuredsCount}`,
+        `救助率: ${rescueRate * 100}%`
+      ],
       missions.map(mission => {
         const { startedAt, finishedAt } = mission
         return [
@@ -101,13 +114,8 @@ export const result = (project: Project, environment: Environment): string => {
             `${startedAt.toLocaleString()} → ${finishedAt.toLocaleString()}`,
             mission.tasks.map(({ type, startedAt, finishedAt, transport, injuredsCount }) => {
               const messages = [taskTypeToLabel(type)]
-
-              if (injuredsCount) {
-                messages.push(`負傷者数: ${injuredsCount}`)
-              }
-
+              if (injuredsCount) messages.push(`負傷者数: ${injuredsCount}`)
               messages.push(`${startedAt.toLocaleString()} → ${finishedAt.toLocaleString()} (${transport.displayName})`)
-
               return messages
             })
         ]
